4372088b76dbeacc7d94dde324732a39e279c3f7
Jouleslabs <muhin.cse.diu@gmail.com>
2020-02-14 17:58:45 +0600

    added loader

--- woo-free-product-sample.php
+++ woo-free-product-sample.php
@@ -31,11 +31,18 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
+// WC active check
+if ( ! is_woocommerce_active() ) {
+	return;
+}
+
 define( 'WFPS_VERSION', '1.1.5' );
+define( 'MINIMUM_PHP_VERSION', '5.6.0' );
+define( 'MINIMUM_WP_VERSION', '4.4' );
+define( 'MINIMUM_WC_VERSION', '3.0.9' );
 define( 'WFPS_URL', plugins_url( '/', __FILE__ ) );
 define( 'WFPS_ADMIN_URL', WFPS_URL . 'admin/' );
 define( 'WFPS_PUBLIC_URL', WFPS_URL . 'public/' );
-
 define( 'WFPS_FILE', __FILE__ );
 define( 'WFPS_ROOT_DIR_PATH', plugin_dir_path( __FILE__ ) );
 define( 'WFPS_ADMIN_DIR_PATH', WFPS_ROOT_DIR_PATH . 'admin/' );
@@ -46,7 +53,7 @@ define( 'WFPS_PUBLIC_PATH', WFPS_ROOT_DIR_PATH . 'public/' );
  * This action is documented in includes/class-woo-free-product-sample-activator.php
  */
 function activate_woo_free_product_sample() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-woo-free-product-sample-activator.php';
+	require_once WFPS_ROOT_DIR_PATH . 'includes/class-woo-free-product-sample-activator.php';
 	Woo_Free_Product_Sample_Activator::activate();	
 }
 
@@ -55,7 +62,7 @@ function activate_woo_free_product_sample() {
  * This action is documented in includes/class-woo-free-product-sample-deactivator.php
  */
 function deactivate_woo_free_product_sample() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-woo-free-product-sample-deactivator.php';
+	require_once WFPS_ROOT_DIR_PATH . 'includes/class-woo-free-product-sample-deactivator.php';
 	Woo_Free_Product_Sample_Deactivator::deactivate();
 }
 
@@ -66,7 +73,7 @@ register_deactivation_hook( __FILE__, 'deactivate_woo_free_product_sample' );
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
  */
-require plugin_dir_path( __FILE__ ) . 'includes/class-woo-free-product-sample.php';
+require WFPS_ROOT_DIR_PATH . 'includes/class-woo-free-product-sample.php';
 
 
 /**
@@ -83,3 +90,333 @@ if ( ! function_exists( 'run_woo_free_product_sample' ) ) {
 	}
 	run_woo_free_product_sample();
 }
+
+
+/**
+ * WooCommerce Measurement Price Calculator loader.
+ *
+ * @since 3.14.0
+ */
+class Woo_Free_Product_Sample_Start {
+
+
+	/** minimum PHP version required by this plugin */
+	const WFPS_VERSION = '5.6.0';
+
+	/** minimum PHP version required by this plugin */
+	const MINIMUM_PHP_VERSION = '5.6.0';	
+
+	/** minimum WordPress version required by this plugin */
+	const MINIMUM_WP_VERSION = '4.4';
+
+	/** minimum WooCommerce version required by this plugin */
+	const MINIMUM_WC_VERSION = '3.0.9';
+
+	/** SkyVerge plugin framework version used by this plugin */
+	// const WFPS_URL = plugins_url( '/', __FILE__ );
+
+	/** the plugin name, for displaying notices */
+	// const WFPS_ADMIN_URL = plugins_url( "/", __FILE__ ) . "admin/";
+
+	// /** minimum PHP version required by this plugin */
+	// const WFPS_PUBLIC_URL = plugins_url( '/', __FILE__ ) . 'public/';
+
+	/** minimum WordPress version required by this plugin */
+	const WFPS_FILE = __FILE__;
+
+	/** minimum WooCommerce version required by this plugin */
+	// const WFPS_ROOT_DIR_PATH = plugin_dir_path( __FILE__ );
+
+	/** SkyVerge plugin framework version used by this plugin */
+	const WFPS_ADMIN_DIR_PATH = WFPS_ROOT_DIR_PATH . 'admin/';
+
+	/** the plugin name, for displaying notices */
+	const WFPS_PUBLIC_PATH = WFPS_ROOT_DIR_PATH . 'public/';
+
+	/** the plugin name, for displaying notices */
+	const PLUGIN_NAME = 'Woo Free Product Sample';	
+
+
+	/** @var \Woo_Free_Product_Sample_Loader single instance of this class */
+	private static $instance;
+
+	/** @var array the admin notices to add */
+	private $notices = array();
+
+	/**
+	 * Loads Measurement Price Calculator.
+	 *
+	 * @since 3.14.0
+	 */
+	protected function __construct() {
+
+		register_activation_hook( __FILE__, array( $this, 'activation_check' ) );
+
+		// handle notices and activation errors
+		add_action( 'admin_init',    array( $this, 'check_environment' ) );
+		add_action( 'admin_init',    array( $this, 'add_plugin_notices' ) );
+		add_action( 'admin_notices', array( $this, 'admin_notices' ), 15 );
+
+		// if the environment check fails, initialize the plugin
+		if ( $this->is_environment_compatible() ) {
+			add_action( 'plugins_loaded', array( $this, 'init_plugin' ) );
+		}
+	}
+
+
+	/**
+	 * Cloning instances is forbidden due to singleton pattern.
+	 *
+	 * @since 3.14.0
+	 */
+	public function __clone() {
+
+		_doing_it_wrong( __FUNCTION__, sprintf( 'You cannot clone instances of %s.', get_class( $this ) ), '3.14.0' );
+	}
+
+
+	/**
+	 * Unserializing instances is forbidden due to singleton pattern.
+	 *
+	 * @since 3.14.0
+	 */
+	public function __wakeup() {
+
+		_doing_it_wrong( __FUNCTION__, sprintf( 'You cannot unserialize instances of %s.', get_class( $this ) ), '3.14.0' );
+	}
+
+
+	/**
+	 * Initializes the plugin.
+	 *
+	 * @internal
+	 *
+	 * @since 3.14.0
+	 */
+	public function init_plugin() {
+
+		if ( ! $this->plugins_compatible() ) {
+			return;
+		}
+
+		// load the main plugin class
+		require_once( plugin_dir_path( __FILE__ ) . 'class-wc-measurement-price-calculator.php' );
+
+		wc_measurement_price_calculator();
+	}
+
+	/**
+	 * Checks the server environment and other factors and deactivates plugins as necessary.
+	 *
+	 * Based on http://wptavern.com/how-to-prevent-wordpress-plugins-from-activating-on-sites-with-incompatible-hosting-environments
+	 *
+	 * @internal
+	 *
+	 * @since 3.14.0
+	 */
+	public function activation_check() {
+
+		if ( ! $this->is_environment_compatible() ) {
+
+			$this->deactivate_plugin();
+
+			wp_die( self::PLUGIN_NAME . ' could not be activated. ' . $this->get_environment_message() );
+		}
+	}
+
+	/**
+	 * Checks the environment on loading WordPress, just in case the environment changes after activation.
+	 *
+	 * @internal
+	 *
+	 * @since 3.14.0
+	 */
+	public function check_environment() {
+
+		if ( ! $this->is_environment_compatible() && is_plugin_active( plugin_basename( __FILE__ ) ) ) {
+
+			$this->deactivate_plugin();
+
+			$this->add_admin_notice( 'bad_environment', 'error', self::PLUGIN_NAME . ' has been deactivated. ' . $this->get_environment_message() );
+		}
+	}
+
+
+	/**
+	 * Adds notices for out-of-date WordPress and/or WooCommerce versions.
+	 *
+	 * @internal
+	 *
+	 * @since 3.14.0
+	 */
+	public function add_plugin_notices() {
+
+		if ( ! $this->is_wp_compatible() ) {
+
+			$this->add_admin_notice( 'update_wordpress', 'error', sprintf(
+				'%s requires WordPress version %s or higher. Please %supdate WordPress &raquo;%s',
+				'<strong>' . self::PLUGIN_NAME . '</strong>',
+				self::MINIMUM_WP_VERSION,
+				'<a href="' . esc_url( admin_url( 'update-core.php' ) ) . '">', '</a>'
+			) );
+		}
+
+		if ( ! $this->is_wc_compatible() ) {
+
+			$this->add_admin_notice( 'update_woocommerce', 'error', sprintf(
+				'%1$s requires WooCommerce version %2$s or higher. Please %3$supdate WooCommerce%4$s to the latest version, or %5$sdownload the minimum required version &raquo;%6$s',
+				'<strong>' . self::PLUGIN_NAME . '</strong>',
+				self::MINIMUM_WC_VERSION,
+				'<a href="' . esc_url( admin_url( 'update-core.php' ) ) . '">', '</a>',
+				'<a href="' . esc_url( 'https://downloads.wordpress.org/plugin/woocommerce.' . self::MINIMUM_WC_VERSION . '.zip' ) . '">', '</a>'
+			) );
+		}
+	}
+
+
+	/**
+	 * Determines if the required plugins are compatible.
+	 *
+	 * @since 3.14.0
+	 *
+	 * @return bool
+	 */
+	private function plugins_compatible() {
+
+		return $this->is_wp_compatible() && $this->is_wc_compatible();
+	}
+
+
+	/**
+	 * Determines if the WordPress compatible.
+	 *
+	 * @since 3.14.0
+	 *
+	 * @return bool
+	 */
+	private function is_wp_compatible() {
+
+		return version_compare( get_bloginfo( 'version' ), self::MINIMUM_WP_VERSION, '>=' );
+	}
+
+
+	/**
+	 * Determines if the WooCommerce compatible.
+	 *
+	 * @since 3.14.0
+	 *
+	 * @return bool
+	 */
+	private function is_wc_compatible() {
+
+		return defined( 'WC_VERSION' ) && version_compare( WC_VERSION, self::MINIMUM_WC_VERSION, '>=' );
+	}
+
+
+	/**
+	 * Deactivates the plugin.
+	 *
+	 * @since 3.14.0
+	 */
+	private function deactivate_plugin() {
+
+		deactivate_plugins( plugin_basename( __FILE__ ) );
+
+		if ( isset( $_GET['activate'] ) ) {
+			unset( $_GET['activate'] );
+		}
+	}
+
+
+	/**
+	 * Adds an admin notice to be displayed.
+	 *
+	 * @internal
+	 *
+	 * @since 3.14.0
+	 *
+	 * @param string $slug the slug for the notice
+	 * @param string $class the css class for the notice
+	 * @param string $message the notice message
+	 */
+	public function add_admin_notice( $slug, $class, $message ) {
+
+		$this->notices[ $slug ] = array(
+			'class'   => $class,
+			'message' => $message
+		);
+	}
+
+
+	/**
+	 * Displays admin notices.
+	 *
+	 * @since 3.14.0
+	 */
+	public function admin_notices() {
+
+		foreach ( $this->notices as $notice_key => $notice ) :
+
+			?>
+			<div class="<?php echo esc_attr( $notice['class'] ); ?>">
+				<p><?php echo wp_kses( $notice['message'], array( 'a' => array( 'href' => array() ) ) ); ?></p>
+			</div>
+			<?php
+
+		endforeach;
+	}
+
+
+	/**
+	 * Determines if the server environment is compatible with this plugin.
+	 *
+	 * Override this method to add checks for more than just the PHP version.
+	 *
+	 * @since 3.14.0
+	 *
+	 * @return bool
+	 */
+	private function is_environment_compatible() {
+
+		return version_compare( PHP_VERSION, self::MINIMUM_PHP_VERSION, '>=' );
+	}
+
+
+	/**
+	 * Gets the message for display when the environment is incompatible with this plugin.
+	 *
+	 * @since 3.14.0
+	 *
+	 * @return string
+	 */
+	protected function get_environment_message() {
+
+		return sprintf( 'The minimum PHP version required for this plugin is %1$s. You are running %2$s.', self::MINIMUM_PHP_VERSION, PHP_VERSION );
+	}
+
+
+	/**
+	 * Gets the main Measurement Price Calculator loader instance.
+	 *
+	 * Ensures only one instance can be loaded.
+	 *
+	 * @since 3.14.0
+	 *
+	 * @return \Woo_Free_Product_Sample_Start
+	 */
+	public static function instance() {
+
+		if ( null === self::$instance ) {
+
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+
+
+}
+
+
+// fire it up!
+Woo_Free_Product_Sample_Start::instance();
