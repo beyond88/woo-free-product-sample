4c0b332198bfc7917df5bf6678e3331ad29c2338
Jouleslabs <muhin.cse.diu@gmail.com>
2020-05-13 14:42:07 +0600

    remove print_r2

--- includes/class-woo-free-product-sample.php
+++ includes/class-woo-free-product-sample.php
@@ -50,7 +50,7 @@ class Woo_Free_Product_Sample {
 		if ( defined( 'WFPS_VERSION' ) ) {
 			$this->version = WFPS_VERSION;
 		} else {
-			$this->version = '2.0.7';
+			$this->version = '2.0.8';
 		}
 		$this->plugin_name = 'woo-free-product-sample';
 
